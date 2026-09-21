3c4fb918689b41e3563012f198e873617cbdad16
patasri-santra <patasrisantra971@gmail.com>
2026-09-21 19:37:45 +0530

    update vercel url

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:5173", "https://your-vercel-url.vercel.app"],
+  origin: ["http://localhost:5173", "https://findmycrew-delta.vercel.app/"],
   credentials: true,
 }));
 app.use(express.json());
